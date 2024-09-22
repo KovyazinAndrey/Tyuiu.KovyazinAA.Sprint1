e6105993c94b5a46d5e8b77d110761424aece422
KrovavyiParen <hiroazora@gmail.com>
2024-09-22 14:13:38 +0500

    Task1

---
+++ Tyuiu.KovyazinAA.Sprint1.Task1.V26.Task/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KovyazinAA.Sprint1.Task1.V26.Task
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- Tyuiu.KovyazinAA.Sprint1.Task1.V26/Program.cs
+++ Tyuiu.KovyazinAA.Sprint1.Task1.V26/Program.cs
@@ -1,5 +1,5 @@
-﻿using Tyuiu.KovyazinAA.Sprint1.Task0.V13.Lib;
-namespace Tyuiu.KovyazinAA.Sprint1.Task0.V13
+﻿using Tyuiu.KovyazinAA.Sprint1.Task1.V26.Lib;
+namespace Tyuiu.KovyazinAA.Sprint1.Task1.V26
 {
     internal class Program
     {
@@ -8,24 +8,29 @@ namespace Tyuiu.KovyazinAA.Sprint1.Task0.V13
             DataService ds = new DataService();
             Console.Title = "Спринт #0 | Выполнил: Ковязин А. А. | ПКТб-24-1";
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
-            Console.WriteLine("* Задание #7                                                              *");
-            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #26                                                              *");
             Console.WriteLine("* Выполнил: Ковязин Андрей Александрович | ПКТб-24-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
-            Console.WriteLine("* одинаковых массивов по длинне                                           *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пльзователя исходные данные,  *");
+            Console.WriteLine("* вычисляет результат по формуле 5 * x / (2 + y) и печатает его на экране.*");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* 24 / (6 * 2) - 24 / 6 / 4                                               *");
+            double x, y;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine(ds.Calculate());
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
         }
     }
 }
